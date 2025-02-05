25e9fcd09eb533fa4993cce365f9e3b3129b7fe9
HYEJIN KWON <chahaa0189@gmail.com>
2025-02-05 21:56:14 +0900

    second-1 commit

--- main.js
+++ main.js
@@ -11,5 +11,27 @@ plusBtn.addEventListener("click",plusClick);
 
 function plusClick(){
    let taskValue = InputId.value
-   console.log(taskValue)
+   taskList.push(taskValue)
+ 
+   render()
+
+}
+
+function render(){
+   let resultHTML = ''
+   for(let i = 0;i<taskList.length;i++){
+      resultHTML+=`<div id="task-board">
+                <div class="tasks">
+                    <div>${taskList[i]}</div>
+                    <div><button>Check</button> <button>Delete</button></div>
+                </div>
+            </div>`
+   }
+
+
+
+
+
+   document.getElementById("task-board").innerHTML = resultHTML;
+
 }
